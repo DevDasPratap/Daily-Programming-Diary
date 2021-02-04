3dc094909682a7d7a371e71341e175f9e360e486
Pratap Das <59955072+epratap@users.noreply.github.com>
2021-02-04 09:53:21 +0530

    Create Day10.cpp

---
+++ Day10.cpp
@@ -0,0 +1,16 @@
+int repeatedStringMatch(string a, string b)
+{
+   string p=a;
+        int i=1;
+        while(i<=(b.length()/a.length())+2)   
+        {
+            if(p.find(b)!=-1)
+            {
+                break;
+            }
+            else 
+                p+=a;
+            i++;    
+        }
+        return (p.find(b)!=-1)?i:-1;
+}
