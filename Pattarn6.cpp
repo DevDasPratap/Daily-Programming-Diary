be2b29505c58eda9244432debd9eab66b2f0a787
Pratap Das <59955072+epratap@users.noreply.github.com>
2021-04-17 02:17:40 +0530

    Create Pattarn6.cpp

---
+++ Pattarn6.cpp
@@ -0,0 +1,41 @@
+//Pattarn
+#include<iostream>
+using namespace std;
+
+int main(){
+int row;
+
+cout<<"Enter a number: ";
+cin>>row;
+
+for(int i=1; i<=row; i++){
+    for(int j=1; j<=i; j++){
+        cout<<"*";
+    }
+    int space=2*row - 2*i;
+    for(int j=1; j<=space; j++){
+        cout<<" ";
+    }
+    for(int j=1; j<=i; j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+for(int i=row; i>=1; i--){
+    for(int j=1; j<=i; j++){
+        cout<<"*";
+    }
+    int space=2*row - 2*i;
+    for(int j=1; j<=space; j++){
+        cout<<" ";
+    }
+    for(int j=1; j<=i; j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+system("pause");
+return 0;
+}
