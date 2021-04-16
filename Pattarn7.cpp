647119430f7a240cba8d8b0bf2c23f7017e454fc
Pratap Das <59955072+epratap@users.noreply.github.com>
2021-04-17 02:18:03 +0530

    Create Pattarn7.cpp

---
+++ Pattarn7.cpp
@@ -0,0 +1,23 @@
+
+//Pattarn
+#include<iostream>
+using namespace std;
+
+int main(){
+int row;
+
+cout<<"Enter a number: ";
+cin>>row;
+
+for(int i=1; i<=row; i++){
+    for(int j=1; j<=row+1-i; j++){
+        cout<<j<<" ";
+    }
+   cout<<endl;
+}
+
+
+
+system("pause");
+return 0;
+}
