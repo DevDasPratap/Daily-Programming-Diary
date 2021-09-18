b5576191a75830ab61674ea1c7ac48d6fafc2af2
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2021-09-18 16:55:44 +0530

    Update Calculate-Power-of-a-Number.cpp

--- Calculate-Power-of-a-Number.cpp
+++ Calculate-Power-of-a-Number.cpp
@@ -10,3 +10,24 @@ int main() {
   cout<<result;  
     return 0;
 }
+
+
+//Calculate Power of a Numberwithout using inbuilt pow()
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int base, exponent, result=1;
+  cout<<"Enter numbers: ";
+  cin>>base>>exponent;
+  cout<<base<<exponent;
+  while(exponent > 0){
+    result *= base;
+    --exponent;
+  }
+  cout<<result;  
+    return 0;
+}
