e0154e12c639ae1d16ed3e3dc87fc9c071bdfb96
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2021-10-01 15:21:06 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>  
+using namespace std;
+
+int binarySearch(int arr[], int num, int key){
+  int startPoint=0, endPoint=num;
+  while(startPoint<=endPoint){
+    int middlePoint=(startPoint+endPoint)/2;
+    if(arr[middlePoint]==key){
+      return middlePoint;
+    }else if(arr[middlePoint]>key){
+      endPoint=middlePoint-1;
+    }else{
+      startPoint=middlePoint+1;
+    }
+  }
+  return -1;
+}
+int main()  
+{
+  int no, sum=0;
+  cout<<"Enter size of array: ";
+  cin>>no;
+  int arr[no];
+  for(int i=0; i<=no; i++){
+    cout<<"Enter array iteam: "<<endl;
+    cin>>arr[i];
+  }
+  int key;
+  cout<<"Enter find key in this array: ";
+  cin>>key;
+  cout<<binarySearch(arr, no, key)<<endl;
+  
+  return 0;
+}  
