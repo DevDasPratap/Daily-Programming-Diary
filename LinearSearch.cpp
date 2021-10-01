ba56734860e545dcd7c8eac28805aa1c1c9dfa39
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2021-10-01 15:06:20 +0530

    Create LinearSearch.cpp

---
+++ LinearSearch.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>  
+using namespace std;
+
+int linearSearch(int arr[], int n, int key){
+  for(int i=0; i<=n; i++){
+    if(arr[i]==key){
+      return i;
+    }
+  }
+  return -1;
+}
+int main()  
+{
+  int no, sum=0;
+  cout<<"Enter no of array: ";
+  cin>>no;
+  int arr[no];
+  for(int i=0; i<=no; i++){
+    cout<<"Enter array iteam: "<<endl;
+    cin>>arr[i];
+  }
+  int key;
+  cout<<"Enter find key in this array: ";
+  cin>>key;
+  cout<<linearSearch(arr, no, key)<<endl;
+  
+  return 0;
+}  
