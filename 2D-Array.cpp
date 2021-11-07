65dc9428a71e92bced8b6435776eaf452441a86b
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2021-11-07 17:05:01 +0530

    Update 2D-Array.cpp

--- 2D-Array.cpp
+++ 2D-Array.cpp
@@ -5,11 +5,14 @@ int main(){
 //2D array
 
 int row, col;
-cout << "Enter numbers of row and column: " << endl;
-cin>>row>>col;
+cout << "Enter numbers of row: ";
+cin>>row;
+cout << "Enter numbers of column: ";
+cin>>col;
 int numbers[row][col];
 for (int i = 0; i <row; ++i) {
         for (int j = 0; j <col; ++j) {
+          cout<<"Enter row number: "<<i<<" and column number "<<j<<" array item: ";
             cin >> numbers[i][j];
         }
     }
