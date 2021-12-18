0eba444d2cae2338608975990e2ea18a8e4722ab
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2021-12-18 20:19:18 +0530

    Update FindLargeSmallinArray.cpp

--- FindLargeSmallinArray.cpp
+++ FindLargeSmallinArray.cpp
@@ -24,3 +24,29 @@ int main(){
 
     return 0;
 }
+
+
+//another way
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int no;
+    cout<<"Enter arr size: ";
+    cin>>no;
+    int arr[no];
+    for(int i=0; i<=no; i++){
+        cin>>arr[i];
+    }
+    int large=INT_MIN;
+    int small=INT_MAX;
+    for(int i=0; i<=no; i++){
+        large=max(large, arr[i]);
+        small=min(small, arr[i]);
+    }
+    cout<<large<<" is large"<<endl;
+    cout<<small<< " is small"<<endl;
+
+    return 0;
+}
