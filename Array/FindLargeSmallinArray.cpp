fa0cf747359df705ee893c943eed34bde7945c42
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2021-12-27 10:21:22 +0530

    Update FindLargeSmallinArray.cpp

--- Array/FindLargeSmallinArray.cpp
+++ Array/FindLargeSmallinArray.cpp
@@ -1,1 +1,27 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int size;
+    cout<<"Enter arr size: ";
+    cin>>size;
+    int arr[size];
+    for(int i=0; i<size; i++){ 
+        cout<<"Enter array element: ";
+        cin>>arr[i];
+    }
+    int min=INT_MAX;
+    int max=INT_MIN;
+    for(int i=0; i<size; i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    cout<<min<<" is smalest number in array"<<endl;
+    cout<<max<<" is largest number in array"<<endl;
+    return 0;
+}
