104d9109682446a29a454bb7d124cb3b91bb21f2
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2021-12-29 21:05:32 +0530

    Create InbuiltSortDecresing.cpp

---
+++ Array/InbuiltSortDecresing.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+bool sortCompare(int a, int b){
+    return a>b;
+}
+int main(){
+    int size;
+    cout<<"Enter array size: ";
+    cin>>size;
+    int arr[size];
+    for(int i=0; i<size; i++){
+        cout<<"Enter index ["<<i<<"] element: ";
+        cin>>arr[i];
+    }
+    sort(arr, arr+size, sortCompare);
+    for(int i=0; i<size; i++){
+        cout<<"Sorting array index ["<<i<<"] element is: "<<arr[i]<<endl;
+    }
+    return 0;
+}
