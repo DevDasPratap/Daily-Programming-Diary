b76f8aa3a0282b1f24899d5e7ca6d7b09487e574
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-01-02 15:34:46 +0530

    Create VectorPushBack.cpp

---
+++ Standard Template Library (STL)/VectorPushBack.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printVector(vector<int>v){
+    cout<<"Size: "<<v.size()<<endl;
+    for(int i=0; i<v.size(); i++){
+        cout<<"Vector index no ["<<i<<"] and element is: "<<v[i]<<endl;
+    };
+}
+ int main(){
+    vector<int>v;
+    int size;
+    cout<<"Enter size of vector: ";
+    cin>>size;
+    for(int i=0; i<size; i++){
+        int value;
+        printVector(v);
+        cout<<"Enter index no ["<<i<<"] enter value: ";
+        cin>>value;
+        v.push_back(value);
+    }
+    v.push_back(10);
+    printVector(v);
+     return 0;
+ }
