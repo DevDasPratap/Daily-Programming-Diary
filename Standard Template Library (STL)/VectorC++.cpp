4571095ddbd31444640f1840a1d1d8f31039e448
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-01-02 14:58:19 +0530

    Create VectorC++.cpp

---
+++ Standard Template Library (STL)/VectorC++.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printVector(vector<int>v){
+    cout<<"Size: "<<v.size()<<endl;
+    for(int i=0; i<v.size(); i++){
+        cout<<"Vector index no ["<<i<<"] and element is: "<<v[i]<<endl;
+    };
+}
+ int main(){
+    vector<int>v;
+    int size;
+    cout<<"Enter size of vector: ";
+    cin>>size;
+    for(int i=0; i<size; i++){
+        int value;
+        cout<<"Enter index no ["<<i<<"] enter value: ";
+        cin>>value;
+        printVector(v);
+        v.push_back(value);
+    }
+    printVector(v);
+     return 0;
+ }
