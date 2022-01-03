932b9e039fdaa6837598a4a4c3c0c564a3dd9484
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-01-04 03:59:34 +0530

    Create StringSort.cpp

---
+++ Standard Template Library (STL)/StringSort.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int size;
+    cout<<"Enter array size: ";
+    cin>>size;
+    vector<string> arr(size);
+    for(int i=0; i<size; i++){
+        cout<<"Enter array index ["<<i<<"] element is: ";
+        cin>>arr[i];
+    }
+    //Start to end
+    // reverse(arr.begin()+2, arr.end());
+    // reverse(arr.begin(), arr.end()+2);
+    reverse(arr.begin(), arr.end());
+    for(int i=0; i<size; i++){
+        cout<<"Sorting array index ["<<i<<"] element is: "<<arr[i]<<endl;
+    }
+    return 0;
+}
