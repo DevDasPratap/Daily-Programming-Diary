ed8103a2a366d9da42e0ee26143842664ed71161
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-01-27 08:36:51 +0530

    Create Encapsulation.cpp

---
+++ OOPs/Encapsulation.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Encapsulation
+class student{
+    string name;
+    int age;
+    int rollNum;
+
+    public:
+    int getAge(){
+        return this->age;
+    }
+};
+int main(){
+
+    student first;
+    cout<<"Run encapsulation code"<<endl;
+
+    return 0;
+};
