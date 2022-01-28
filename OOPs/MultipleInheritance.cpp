2877e8a38c779d04f3ca29784377471091e3d241
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-01-29 00:26:16 +0530

    Create MultipleInheritance.cpp

---
+++ OOPs/MultipleInheritance.cpp
@@ -0,0 +1,18 @@
+//Multiple Inheritance
+class animal{
+    public:
+    int age;
+    int weight;
+    public:
+    void speak(){
+        cout<<"Speaking"<<endl;
+    }
+};
+class human{
+    public:
+    string color;
+    public:
+    void talk(){
+        cout<<"Talking"<<endl;
+    }
+};
