a63472f973ec824d75c6bc01f2c8016ce66c00df
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-03 02:26:25 +0530

    Create constructor.cpp

---
+++ OOPs/constructor.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class constructorDemo{
+    public:
+    constructorDemo(){
+        cout<<"This is from constructor"<<endl;
+    }
+};
+int main(){
+    constructorDemo obj;
+    return 0;
+}
