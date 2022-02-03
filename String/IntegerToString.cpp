c869b2884a58ee9c07a5e68414b0bbca6a5ddad4
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 02:29:19 +0530

    Create IntegerToString.cpp

---
+++ String/IntegerToString.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //Integer to string
+    int n;
+    cin>>n;
+    cout<<"Integer to string: "<<to_string(n) + "4"<<endl;
+    return 0;
+}
