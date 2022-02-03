af06797e5a554ee68cb796befa0298e305ae3ecf
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 00:43:15 +0530

    Create Switch.cpp

---
+++ Switch.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    char button;
+    cin>>button;
+    switch (button)
+    {
+    case 'a':
+        cout<<"Hello";
+        break;
+    case 'b':
+        cout<<"Hola";
+        break;
+    case 'c':
+        cout<<"Hi";
+        break;
+    default:
+    cout<<"Namaskar.........";
+        break;
+    }
+    return 0;
+}
