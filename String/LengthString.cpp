812f64f925085e4a15294e94400e6854607a7cb9
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 02:11:08 +0530

    Create LengthString.cpp

---
+++ String/LengthString.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //Length string
+    string s;
+    getline(cin, s);
+    cout<<"Size of String: "<<s.size()<<endl;
+    return 0;
+}
