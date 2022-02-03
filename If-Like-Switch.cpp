a6f7321c40c21fc8f0c11351849f733b3b9cfba6
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 00:39:53 +0530

    Create If-Like-Switch.cpp

---
+++ If-Like-Switch.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    char button;
+    cin>>button;
+    if(button=='a'){
+        cout<<"Hello";
+    }else if(button=='b'){
+        cout<<"Holla";
+    }else if(button=='c'){
+        cout<<"Hi";
+    }else if(button=='d'){
+        cout<<"P D";
+    }
+    return 0;
+}
