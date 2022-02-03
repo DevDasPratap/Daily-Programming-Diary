685277549c43664cc5a3b668f914baded25fab60
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 02:05:51 +0530

    Create FindString.cpp

---
+++ String/FindString.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //Find string
+    string s;
+    getline(cin, s);
+    cout<<s<<endl;
+    cout<<"Find: "<<s.find("das")<<endl;
+    return 0;
+}
