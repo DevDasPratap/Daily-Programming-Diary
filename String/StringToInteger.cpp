8bc96fb68aa628e876453f3cb713d4f75578dcad
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 02:26:10 +0530

    Create StringToInteger.cpp

---
+++ String/StringToInteger.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //string to integer
+    string s;
+    getline(cin, s);
+    int str=stoi(s);
+    cout<<"String to integer: "<<str+4<<endl;
+    return 0;
+}
