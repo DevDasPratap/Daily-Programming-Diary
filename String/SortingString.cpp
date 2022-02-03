bba9e6dd2626066f2105a7210781b34a8688c14a
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 02:32:24 +0530

    Create SortingString.cpp

---
+++ String/SortingString.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //Sorting string
+    string s;
+    getline(cin, s);
+    sort(s.begin(), s.end());
+    cout<<"Sorting string: "<<s<<endl;
+    return 0;
+}
