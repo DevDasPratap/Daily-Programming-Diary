e39881c3d32838fe444b0901d9c493fe9b7caf68
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 02:20:30 +0530

    Create SubStringString.cpp

---
+++ String/SubStringString.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //SubString string
+    string s;
+    getline(cin, s);
+    string str=s.substr(4, 5);
+    cout<<"SubString: "<<str<<endl;
+    return 0;
+}
