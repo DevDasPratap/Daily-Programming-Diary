a24ad5c191d02cca4d56eacf485ccdd67fbc6e63
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 01:39:24 +0530

    Create ClearFromString.cpp

---
+++ String/ClearFromString.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //Clear from string
+    string s;
+    getline(cin, s);
+    cout<<s<<endl;
+    s.clear();
+    cout<<"Clear now: "<<s<<endl;
+    return 0;
+}
