84b2d347b1c7a1259126bac398aa110cde8c9034
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 01:48:00 +0530

    Create CompaireFromTwoString.cpp

---
+++ String/CompaireFromTwoString.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //Compaire from two string
+    string s1, s2;
+
+    // s1="abc";
+    // s2="xyz";
+    
+    getline(cin, s1);
+    getline(cin, s2);
+    cout<<s1.compare(s2)<<endl;
+    return 0;
+}
