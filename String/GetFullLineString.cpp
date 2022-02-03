392e808925cf92efd15ba8e64ca8abbae505232b
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-04 01:32:44 +0530

    Create GetFullLineString.cpp

---
+++ String/GetFullLineString.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string str;
+    //Get full line 
+    getline(cin, str);
+     cout<<"Your enter string: "<<str<<endl;
+    return 0;
+}
