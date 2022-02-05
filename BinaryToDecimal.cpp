325db14c5e71eb3a9cff8b5ad49d02be59e03a04
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-06 02:28:32 +0530

    Create BinaryToDecimal.cpp

---
+++ BinaryToDecimal.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int ans= __builtin_popcount(n);
+    cout<<"Binary code to desimel: "<<ans<<endl;
+    return 0;
+};
