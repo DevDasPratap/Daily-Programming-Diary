822e0c2075eca12821b7c8dcd13bc0b6e833f4f3
Pratap Das <59955072+DevDasPratap@users.noreply.github.com>
2022-02-06 02:22:44 +0530

    Update next_permutation.cpp

--- String/next_permutation.cpp
+++ String/next_permutation.cpp
@@ -12,3 +12,21 @@ int main(){
     
     return 0;
 };
+
+//or
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin, s);
+    do
+    {
+        cout<<s<<endl;
+    } while (next_permutation(s.begin(), s.end()));
+    cout<<s<<endl;
+    
+    return 0;
+};
